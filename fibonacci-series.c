a0ba5eedb7c334dfa3a121e73dd9d3045d243133
THIRUKKURAL SELVAN <akauriht@gmail.com>
2023-01-20 11:02:43 +0530

    FIbonacci Series - Normal Approach

---
+++ fibonacci-series.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+// 0 1 1 2 3 5 8
+int main()
+{
+    int n = 0;
+    scanf("%d", &n);
+    int num1 = 0;
+    int num2 = 1;
+    printf("%d %d", num1, num2);
+    int num3;
+
+    while (n)
+    {
+
+        num3 = num1 + num2;
+        printf(" %d", num3);
+        num1 = num2;
+        num2 = num3;
+        n--;
+    }
+
+    return EXIT_SUCCESS;
+}
