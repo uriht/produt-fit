7c6602b48e7cd10bb01c845914f8e51c1a2f8ce8
THIRUKKURAL SELVAN <akauriht@gmail.com>
2023-01-23 14:09:29 +0530

    Code Updated

---
+++ rearrange-odd-even.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+// 7.Given a number 'n' - rearrange the number, first odd digits and then even digits, print the sum of original number and rearranged number 123456 135246 + 123456 = Answer
+
+int main()
+{
+    printf("%d", EXIT_SUCCESS);
+    return EXIT_SUCCESS;
+}
